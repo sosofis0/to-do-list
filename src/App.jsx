0680e5e9d17156ffce8e15f9c5737f290c9919b9
sosofis0 <sofikorn16@gmail.com>
2026-01-15 10:06:55 -0300

    Modifição no css e adição do formulário

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,8 @@
 import { useState } from 'react';
+import Todo from './components/Todo';
+import TodoForm from './components/TodoForm';
 import './App.css';
+
 
 
 function App() {
@@ -25,24 +28,24 @@ function App() {
   ])
 
 
-  return <div className='app'>
+  return( 
+  <div className='app'>
+    
     <h1>Lista de Tarefas</h1>
+    
     <div className='todo-list'>
+      
       {todos.map((todo) => (
-        <div className='todo'>
-          <div className='content'>
-            <p>{todo.text}</p>
-            <p className="category">({todo.category})</p>
-          </div>
-          <div>
-            <button>Completar</button>
-            <button>x</button>
-          </div>
-        </div>
+        
+        <Todo todo={todo}/>
+      
       ))}
+   
     </div>
-
-  </div>
+    
+    <TodoForm/>
+  
+  </div>)
 }
 
 export default App
