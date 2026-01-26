8030e96cf48879fb5703cfe0cc4169ab21904851
sosofis0 <sofikorn16@gmail.com>
2026-01-26 10:42:44 -0300

    criação da funcionalidade de pesquisa e da funcionalidade de completar e excluir tarefa

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,9 @@
 import { useState } from 'react';
 import Todo from './components/Todo';
 import TodoForm from './components/TodoForm';
+import Search from './components/Search';
 import './App.css';
+
 
 
 
@@ -28,6 +30,8 @@ function App() {
   ]
 );
 
+const [search,setSearch] = useState("");
+
 const addTodo = (text, category) => {
   const newTodos = [
     ...todos,
@@ -42,17 +46,32 @@ const addTodo = (text, category) => {
   setTodos(newTodos);
 };
 
+const removeTodo =(id) => {
+  const newTodos =[...todos];
+  const filteredTodos = newTodos.filter((todo) => 
+    todo.id !== id ? todo : null
+  );
+  setTodos(filteredTodos);
+} 
+
+const completeTodo = (id) => {
+  const newTodos = [...todos]
+  newTodos.map((todo) => 
+    todo.id === id ? (todo.isCompleted = !todo.isCompleted) : todo);
+  setTodos(newTodos);
+}
+
   return( 
   <div className='app'>
     
     <h1>Lista de Tarefas</h1>
+    <Search search={search} setSearch={setSearch}/>
     
     <div className='todo-list'>
       
-      {todos.map((todo) => (
+      {todos.filter((todo) => todo.text.toLowerCase().includes(search.toLowerCase())).map((todo) => (
         
-        <Todo key={todo.id} todo={todo}/>
-      
+        <Todo key={todo.id} todo={todo} removeTodo ={removeTodo} completeTodo = {completeTodo}/>
       ))}
    
     </div>
